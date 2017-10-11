c2510e49b82d0c29e7fb1a55cd98bac3bd83f3b5
AraqelAraqelyan <AraqelAraqelyan@gmail.com>
2017-10-11 16:12:25 +0000

    working

--- vzgo.py
+++ vzgo.py
@@ -9,18 +9,22 @@ GPIO.setup(pin, GPIO.OUT)
 
 pwm=GPIO.PWM(pin, 50)
 
-pwm.start(0)
+pwm.start(11)
 
 def SetAngle(angle):
-	duty = angle / 18 + 2
+	duty = angle/18+2
 	GPIO.output(pin, True)
+	print(duty)
 	pwm.ChangeDutyCycle(duty)
-	sleep(1)
+	sleep(5)
 	GPIO.output(pin, False)
-	pwm.ChangeDutyCycle(0)
+#	pwm.ChangeDutyCycle(0)
 
-for angle in range(0,20):
-	SetAngle(angle)
+
+SetAngle(180)
+#SetAngle(0)
+#for angle in range(0,20):
+#	SetAngle(angle)
 
 
 pwm.stop()
